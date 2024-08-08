4288036ce71a97d0b1e6b5a4b6ed8db63f960f0a
Julyana Gusmao <the2kira@hotmail.com>
2024-08-08 07:50:23 -0300

    edit profile and verifications

--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -1,74 +1,153 @@
 import logotype from '@utils/logotype.png';
-import React, { useState } from 'react';
+import calendar from '@utils/calendar.svg';
+import React, { useState, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import InputMask from 'react-input-mask';
 import api from './../../services/api';
 
 const Register: React.FC = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [birthDate, setBirthDate] = useState('');
+  const [birthDate, setBirthDate] = useState<Date | null>(null);
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
+  const datePickerRef = useRef<DatePicker>(null);
 
   const handleSignUp = async () => {
+    if (!firstName || !lastName || !birthDate || !phone || !email || !password) {
+      setErrorMessage('Por favor, preencha todos os campos');
+      return;
+    }
+
+    if (firstName.length < 3 || firstName.length > 30) {
+      setErrorMessage('O nome deve ter entre 3 e 30 caracteres');
+      return;
+    }
+
+    if (lastName.length < 3 || lastName.length > 30) {
+      setErrorMessage('O sobrenome deve ter entre 3 e 30 caracteres');
+      return;
+    }
+
+    if (email.length < 3 || email.length > 50) {
+      setErrorMessage('O email deve ter entre 3 e 50 caracteres');
+      return;
+    }
+
+    if (password.length < 6 || password.length > 20) {
+      setErrorMessage('A senha deve ter entre 6 e 20 caracteres');
+      return;
+    }
+
     try {
       await api.post('/auth/register', {
         first_name: firstName,
         last_name: lastName,
-        birth_date: birthDate,
+        birth_date: birthDate ? birthDate.toISOString().split('T')[0] : null,
         phone,
         email,
         password,
       });
       navigate('/');
-    } catch (error) {
-      console.error('Erro ao se cadastrar:', error);
+    } catch (error: any) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setErrorMessage('Esse email já possui conta registrada no Simmers');
+      } else {
+        setErrorMessage('Erro ao se cadastrar, tente novamente.');
+      }
     }
   };
 
   return (
-
     <section className='flex justify-between h-screen'>
-
       <div className='bg-darkGreen w-1/3 flex flex-col justify-between py-6 px-10'>
         <img src={logotype} alt='logotype' width="200" />
-
-        <div id='handleForm' className='flex flex-col gap-5'>
+        <div id='handleForm' className='flex flex-col gap-5 [&>input]:w-full items-center'>
           <input
+            required
             type="text"
+            minLength={3}
+            maxLength={30}
             placeholder="Nome"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
           <input
+            required
             type="text"
+            minLength={3}
+            maxLength={30}
             placeholder="Sobrenome"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           />
-          <input
-            type="date"
-            placeholder="Data de Nascimento"
-            value={birthDate}
-            onChange={(e) => setBirthDate(e.target.value)}
-          />
-          <input
-            type="tel"
-            placeholder="Telefone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
+          <div className='flex gap-5'>
+            <div className='relative'>
+              <DatePicker
+                selected={birthDate}
+                onChange={(date: Date | null) => setBirthDate(date)}
+                dateFormat="dd-MM-yyyy"
+                maxDate={new Date()}
+                showYearDropdown
+                showMonthDropdown
+                scrollableYearDropdown
+                yearDropdownItemNumber={100}
+                placeholderText="00-00-0000"
+                customInput={
+                  <InputMask
+                    mask="99-99-9999"
+                    value={birthDate ? birthDate.toLocaleDateString('en-GB') : ''}
+                    onChange={(e) => {
+                      const dateParts = e.target.value.split('-');
+                      if (dateParts.length === 3) {
+                        const [day, month, year] = dateParts.map(part => parseInt(part, 10));
+                        setBirthDate(new Date(year, month - 1, day));
+                      } else {
+                        setBirthDate(null);
+                      }
+                    }}
+                    className="input"
+                    placeholder="00-00-0000"
+                  />
+                }
+                ref={datePickerRef}
+              />
+              <img
+                src={calendar}
+                alt="Calendar"
+                className="w-5 h-5 absolute right-3 top-[10px] cursor-pointer"
+                onClick={() => datePickerRef.current?.setOpen(true)}
+              />
+            </div>
+            <InputMask
+              required
+              mask="(99) 99999-9999"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder="Telefone"
+              className="input"
+            />
+          </div>
           <div className='flex flex-col gap-4 text-center mt-2 border-2 p-3 border-lightGreen'>
             <p className='text-white'>Obs: Essas serão suas credenciais para fazer login</p>
             <input
+              required
+              minLength={3}
+              maxLength={50}
               type="email"
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
             <input
+              required
+              minLength={6}
+              maxLength={20}
               type="password"
               placeholder="Senha"
               value={password}
@@ -76,16 +155,17 @@ const Register: React.FC = () => {
             />
           </div>
         </div>
-        <button onClick={handleSignUp} className='btn-secondary hover:bg-lightGreen'>Cadastrar</button>
-        
+        <div className='flex flex-col gap-3'>
+          {errorMessage && <p className="text-red-700 text-center">{errorMessage}</p>}
+          <button onClick={handleSignUp} className='btn-secondary hover:bg-lightGreen'>Cadastrar</button>
+        </div>
         <div className='flex gap-3 items-center ml-24'>
           <p className='text-white'>Já é cadastrado?</p>
           <Link to="/login" className='text-white font-medium'>
-              Entrar agora
+            Entrar agora
           </Link>
         </div>
       </div>
-
       <div className='bg-register-bg bg-cover bg-no-repeat bg-center w-2/3 text-center relative'>
         <div className='absolute top-16 left-52 space-y-4 text-center'>
           <h1 className='text-5xl font-bold'>Entre no mundo dos sims<br />e faça amigos!</h1>
--- frontend/src/pages/Auth/UpdateProfile.tsx
+++ frontend/src/pages/Auth/UpdateProfile.tsx
@@ -1,23 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import api from '../../services/api';
 import arrowBack from '@utils/arrow-back.svg';
+import calendar from '@utils/calendar.svg';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import InputMask from 'react-input-mask';
 
 const UpdateProfilePage: React.FC = () => {
   const { user: currentUser, updateUser, signOut } = useAuth();
   const navigate = useNavigate();
   const [firstName, setFirstName] = useState<string>('');
   const [lastName, setLastName] = useState<string>('');
-  const [birthDate, setBirthDate] = useState<string>('');
+  const [birthDate, setBirthDate] = useState<Date | null>(null);
+  const [phone, setPhone] = useState<string>('');
   const [profilePicture, setProfilePicture] = useState<File | null>(null);
   const [profilePictureUrl, setProfilePictureUrl] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [currentPassword, setCurrentPassword] = useState<string>('');
+  const [newPassword, setNewPassword] = useState<string>('');
+  const [showEmailInput, setShowEmailInput] = useState<boolean>(false);
+  const [showPasswordInput, setShowPasswordInput] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const datePickerRef = useRef<DatePicker>(null);
 
   useEffect(() => {
     if (currentUser) {
       setFirstName(currentUser.firstName);
       setLastName(currentUser.lastName);
-      setBirthDate(currentUser.birthDate);
+      setBirthDate(currentUser.birthDate ? new Date(currentUser.birthDate) : null);
+      setPhone(currentUser.phone || '');
+      setEmail(currentUser.email || '');
       setProfilePictureUrl(currentUser.profilePicture ? `http://localhost:3333${currentUser.profilePicture}` : 'default-avatar.png');
     }
   }, [currentUser]);
@@ -25,13 +39,28 @@ const UpdateProfilePage: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    if (firstName.length < 3 || firstName.length > 30) {
+      setErrorMessage('O nome deve ter entre 3 e 30 caracteres');
+      return;
+    }
+
+    if (lastName.length < 3 || lastName.length > 30) {
+      setErrorMessage('O sobrenome deve ter entre 3 e 30 caracteres');
+      return;
+    }
+
     const formData = new FormData();
     formData.append('firstName', firstName);
     formData.append('lastName', lastName);
-    formData.append('birthDate', birthDate);
+    formData.append('birthDate', birthDate ? birthDate.toISOString().split('T')[0] : '');
+    formData.append('phone', phone);
 
     if (profilePicture) {
       formData.append('profilePicture', profilePicture);
+    }
+
+    if (showEmailInput) {
+      formData.append('email', email);
     }
 
     try {
@@ -40,6 +69,13 @@ const UpdateProfilePage: React.FC = () => {
           'Content-Type': 'multipart/form-data',
         },
       });
+
+      if (showPasswordInput && currentPassword && newPassword) {
+        await api.put(`/users/password/${currentUser?.id}`, {
+          currentPassword,
+          newPassword
+        });
+      }
 
       if (profilePicture) {
         const pictureFormData = new FormData();
@@ -56,8 +92,13 @@ const UpdateProfilePage: React.FC = () => {
       }
 
       navigate('/profile');
-    } catch (error) {
+    } catch (error: any) {
       console.error('Error updating profile:', error);
+      if (error.response && error.response.data && error.response.data.message) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage('Erro ao atualizar perfil, tente novamente.');
+      }
     }
   };
 
@@ -76,12 +117,12 @@ const UpdateProfilePage: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center gap-4">
-       <button
-          className="size-10 absolute bottom-44 left-10"
-          onClick={() => navigate(-1)}
-        >
-          <img src={arrowBack} alt="Voltar" />
-        </button>
+      <button
+        className="size-10 absolute bottom-44 left-10"
+        onClick={() => navigate(-1)}
+      >
+        <img src={arrowBack} alt="Voltar" />
+      </button>
       <h1 className="text-2xl font-bold">Edit Profile</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <div className="flex flex-col items-center">
@@ -110,52 +151,146 @@ const UpdateProfilePage: React.FC = () => {
             />
           </label>
         </div>
-        <div>
-          <label className="text-lg font-medium">First Name</label>
+        <div className='flex flex-col'>
+          <label className="text-lg font-medium">Nome</label>
           <input
             type="text"
+            minLength={3}
+            maxLength={30}
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             required
-            className="border border-gray-300 p-2 rounded"
+            className="border border-gray-300 p-2 rounded-md"
           />
         </div>
-        <div>
-          <label className="text-lg font-medium">Last Name</label>
+        <div className='flex flex-col'>
+          <label className="text-lg font-medium">Sobrenome</label>
           <input
             type="text"
+            minLength={3}
+            maxLength={30}
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             required
-            className="border border-gray-300 p-2 rounded"
+            className="border border-gray-300 p-2 rounded-md"
           />
         </div>
-        <div>
-          <label className="text-lg font-medium">Birth Date</label>
-          <input
-            type="date"
-            value={birthDate}
-            onChange={(e) => setBirthDate(e.target.value)}
-            required
-            className="border border-gray-300 p-2 rounded"
-          />
+        <div id='date birth and tel' className='flex items-center gap-3'>
+          <div className='flex flex-col'>
+            <label className="text-lg font-medium">Data de Nascimento</label>
+            <div className="relative">
+              <DatePicker
+                selected={birthDate}
+                onChange={(date: Date | null) => setBirthDate(date)}
+                dateFormat="dd-MM-yyyy"
+                maxDate={new Date()}
+                showYearDropdown
+                showMonthDropdown
+                scrollableYearDropdown
+                yearDropdownItemNumber={100}
+                placeholderText="00-00-0000"
+                customInput={
+                  <InputMask
+                    mask="99-99-9999"
+                    value={birthDate ? birthDate.toLocaleDateString('en-GB') : ''}
+                    onChange={(e) => {
+                      const dateParts = e.target.value.split('-');
+                      if (dateParts.length === 3) {
+                        const [day, month, year] = dateParts.map(part => parseInt(part, 10));
+                        setBirthDate(new Date(year, month - 1, day));
+                      }
+                    }}
+                    className="border border-gray-300 p-2 rounded-md w-full"
+                  />
+                }
+                ref={datePickerRef}
+              />
+              <img 
+                src={calendar} 
+                alt="Calendar" 
+                className="w-5 h-5 absolute right-3 top-[10px] cursor-pointer" 
+                onClick={() => datePickerRef.current?.setOpen(true)}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col'>
+            <label className="text-lg font-medium">Telefone</label>
+            <InputMask
+              mask="(99) 99999-9999"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder="Telefone"
+              className="border border-gray-300 p-2 rounded-md"
+            />
+          </div>
         </div>
-        <div className="flex gap-4">
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-            Save
-          </button>
-          <button
-            type="button"
-            onClick={handleDeleteAccount}
-            className="bg-red-500 text-white px-4 py-2 rounded"
-          >
-            Delete Account
-          </button>
+        <div className='flex flex-col'>
+          <label className="text-lg font-medium">Email</label>
+          {showEmailInput ? (
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 p-2 rounded-md"
+            />
+          ) : (
+            <button
+              type="button"
+              onClick={() => setShowEmailInput(true)}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Alterar Email
+            </button>
+          )}
+        </div>
+        <div className='flex flex-col'>
+          <label className="text-lg font-medium">Senha</label>
+          {showPasswordInput ? (
+            <>
+              <input
+                type="password"
+                placeholder="Senha Atual"
+                value={currentPassword}
+                onChange={(e) => setCurrentPassword(e.target.value)}
+                className="border border-gray-300 p-2 rounded-md"
+              />
+              <input
+                type="password"
+                placeholder="Nova Senha"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                className="border border-gray-300 p-2 rounded-md mt-2"
+              />
+            </>
+          ) : (
+            <button
+              type="button"
+              onClick={() => setShowPasswordInput(true)}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Alterar Senha
+            </button>
+          )}
+        </div>
+        <div className='flex flex-col gap-3'>
+          {errorMessage && <p className="text-red-700 text-center">{errorMessage}</p>}
+          <div className="flex gap-4">
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={handleDeleteAccount}
+              className="bg-red-500 text-white px-4 py-2 rounded-md"
+            >
+              Delete Account
+            </button>
+          </div>
         </div>
       </form>
       <button
         onClick={() => navigate('/profile')}
-        className="bg-gray-500 text-white px-4 py-2 rounded mt-4"
+        className="bg-gray-500 text-white px-4 py-2 rounded-md mt-4"
       >
         Back to Profile
       </button>
